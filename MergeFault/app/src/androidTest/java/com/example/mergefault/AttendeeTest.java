723e0870f4d394a94fc1066299fd9ad5fa182648
Howe Zhu <howezhu321@gmail.com>
2024-03-08 14:33:37 -0700

    Added OrganizerTest renamed OrganizerViewEvents

--- MergeFault/app/src/androidTest/java/com/example/mergefault/AttendeeTest.java
+++ MergeFault/app/src/androidTest/java/com/example/mergefault/AttendeeTest.java
@@ -1,5 +1,6 @@
 package com.example.mergefault;
 
+import androidx.annotation.NonNull;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -15,9 +16,22 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class AttendeeTest {
+
+    FirebaseFirestore db;
 
     @Rule
     public ActivityScenarioRule<AttendeeHomeActivity> activityScenarioRule = new ActivityScenarioRule<>(AttendeeHomeActivity.class);
@@ -31,14 +45,14 @@ public class AttendeeTest {
     @Test
     public void testMyEventsClick() {
         onView(withId(R.id.viewMyEventsButton)).perform(click());
-        // need to complete
+        onView(withId(R.id.myEventListView)).perform(click());
     }
+
 
     @Test
     public void testBrowsePostedEventsClick() {
         onView(withId(R.id.browseEventsButton)).perform(click());
         onView(withId(R.id.myEventListView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
-        // please add the tests here which check for the array adapter data
     }
 
     @Test
@@ -47,6 +61,36 @@ public class AttendeeTest {
         onView(withId(R.id.cancelButton)).perform(click());
         onView(withId(R.id.viewMyEventsButton)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
 
+    }
+
+    @Test
+    public void addTestUser(){
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("AttendeeName", "Test Name");
+        user.put("AttendeeEmail", "test@ualberta.ca");
+        user.put("AttendeePhoneNumber", "1234567890");
+        user.put("AttendeeID", "1234567890");
+        user.put("AttendeeProfile", "http://image.com/image");
+        db.collection("events").document("testdoc").collection("attendees").document("1234567890").set(user).addOnSuccessListener(documentReference -> {
+            Log.d("Success", "Test User Added");
+
+        }).addOnFailureListener(e -> {
+            Log.d("Failure", "Test User not Added");
+            Log.d("Error", e.toString());
+        });
+    }
+
+    @Test
+    public void deleteTestUser(){
+        db = FirebaseFirestore.getInstance();
+        db.collection("events").document("testdoc").collection("attendees").document("1234567890").delete().addOnSuccessListener(unused -> {
+            Log.d("Success", "Test User Deleted");
+
+        }).addOnFailureListener(e -> {
+            Log.d("Failure", "Test User not Deleted");
+            Log.d("Error", e.toString());
+        });
     }
 
 }
