ac95cf7d41d066501d4e8a1972efa9bad38bff62
shivansh113 <shivanshd@gmail.com>
2024-03-28 18:00:02 -0600

    fix

--- MergeFault/app/src/main/java/com/example/mergefault/AttendeeSignedUpEventsActivity.java
+++ MergeFault/app/src/main/java/com/example/mergefault/AttendeeSignedUpEventsActivity.java
@@ -1,5 +1,6 @@
 package com.example.mergefault;
 
+import static com.squareup.okhttp.internal.http.HttpDate.format;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
