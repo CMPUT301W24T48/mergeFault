ca3d7d81c4a5e2ddc18fc1d51af58821ecea1c68
“JoshuaChui03” <joshuachui03@gmail.com>
2024-03-30 04:38:47 -0600

    Added the organizer edit events and fixed Uri null bug and added flow to most organizer screens and also finished toast for required inputs

--- MergeFault/app/src/main/java/com/example/mergefault/OrganizerEventOptions.java
+++ MergeFault/app/src/main/java/com/example/mergefault/OrganizerEventOptions.java
@@ -1,32 +1,41 @@
 package com.example.mergefault;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.net.InetSocketAddress;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
 
 public class OrganizerEventOptions extends AppCompatActivity {
 
     private String eventId;
+    private ImageView homeButton;
     private FirebaseFirestore db;
     private DocumentReference eventRef;
     private Event event;
+    private String organizerId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,7 @@ public class OrganizerEventOptions extends AppCompatActivity {
 
         Intent receiverIntent = getIntent();
         eventId = receiverIntent.getStringExtra("EventId");
+        organizerId = receiverIntent.getStringExtra("OrganizerID");
 
         Log.d("eventId", "eventid: " + eventId);
 
@@ -51,16 +61,22 @@ public class OrganizerEventOptions extends AppCompatActivity {
                         Calendar date = Calendar.getInstance();
                         Date dateTime = doc.getDate("DateTime");
                         date.setTime(dateTime);
+                        Integer attendeeLimit = null;
+                        if (doc.getString("AttendeeLimit") != null) {
+                            attendeeLimit = Integer.parseInt(doc.getString("AttendeeLimit"));
+                        }
+                        Uri downloadUrl = Uri.parse(doc.getString("EventPoster"));
                         event = new Event(doc.getString("EventName"),
                                 doc.getString("OrganizerID"),
                                 doc.getString("Location"),
                                 date,
-                                Integer.parseInt(doc.getString("AttendeeLimit")),
-                                Uri.parse(doc.getString("EventPoster")),
+                                attendeeLimit,
+                                downloadUrl,
                                 doc.getString("Description"),
                                 doc.getBoolean("GeoLocOn"),
                                 doc.getId(),
                                 doc.getString("PlaceID"));
+                        new DownloadImageFromInternet((ImageView) findViewById(R.id.eventPosterImageView)).execute(downloadUrl.toString());
                     }
                 }
             }
@@ -72,6 +88,8 @@ public class OrganizerEventOptions extends AppCompatActivity {
         Button sendNotificationsButton = findViewById(R.id.sendNotifButton);
         Button cancelButton = findViewById(R.id.cancelButton);
         Button mapButton = findViewById(R.id.checkinMapButton);
+        Button removeButton = findViewById(R.id.removeEventButton);
+        homeButton = findViewById(R.id.logoImageView);
         /// TODO: 20-03-2024 add map button
 
         // Attendee List button click
@@ -87,8 +105,11 @@ public class OrganizerEventOptions extends AppCompatActivity {
         eventDetailsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(OrganizerEventOptions.this, event_details.class);
+                Intent intent = new Intent(OrganizerEventOptions.this, OrganizerEditEventActivity.class);
+                intent.putExtra("EventId", eventId);
+                intent.putExtra("OrganizerID", organizerId);
                 startActivity(intent);
+                finish();
             }
         });
 
@@ -112,16 +133,85 @@ public class OrganizerEventOptions extends AppCompatActivity {
             }
         });
 
-        //Cancel button click
-        cancelButton.setOnClickListener(new View.OnClickListener(){
+        removeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                eventRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getApplicationContext(),"Event Deleted",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(OrganizerEventOptions.this, OrganizerViewEventsActivity.class);
+                                intent.putExtra("OrganizerID", organizerId);
+                                startActivity(intent);
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(),"Could not delete event",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
                 Intent intent = new Intent(OrganizerEventOptions.this, OrganizerViewEventsActivity.class);
+                intent.putExtra("OrganizerID", organizerId);
                 startActivity(intent);
+                finish();
+            }
+        };
+        OrganizerEventOptions.this.getOnBackPressedDispatcher().addCallback(this, callback);
+
+        //Cancel button click
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrganizerEventOptions.this, OrganizerViewEventsActivity.class);
+                intent.putExtra("OrganizerID", organizerId);
+                startActivity(intent);
+                finish();
+            }
+        });
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrganizerEventOptions.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
 
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+            Toast.makeText(getApplicationContext(), "Please wait, it may take a few seconds...", Toast.LENGTH_SHORT).show();
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
     }
 }
 
--- MergeFault/app/src/main/java/com/example/mergefault/OrganizerViewEventsActivity.java
+++ MergeFault/app/src/main/java/com/example/mergefault/OrganizerViewEventsActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.content.SharedPreferences;
 import android.widget.ListView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +34,7 @@ public class OrganizerViewEventsActivity extends AppCompatActivity {
     private ImageView homeButton;
     private SharedPreferences sharedPreferences;
     private ListView signedUpEventsList;
+    private Button cancelButton;
     private EventArrayAdapter eventArrayAdapter;
 
     private ArrayList<Event> signedUpEventDataList;
@@ -62,6 +65,7 @@ public class OrganizerViewEventsActivity extends AppCompatActivity {
 
         profileImageView = findViewById(R.id.pfpImageView);
         homeButton = findViewById(R.id.imageView);
+        cancelButton = findViewById(R.id.cancelButton);
         signedUpEventsList = findViewById(R.id.myEventListView);
         sharedPreferences = getSharedPreferences("UserProfile", MODE_PRIVATE);
 
@@ -83,6 +87,7 @@ public class OrganizerViewEventsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(OrganizerViewEventsActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
         signedUpEventsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -90,8 +95,10 @@ public class OrganizerViewEventsActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Event selectedEvent = (Event) signedUpEventsList.getItemAtPosition(position);
                 Intent intent = new Intent(OrganizerViewEventsActivity.this, OrganizerEventOptions.class);
-                intent.putExtra("EventId", selectedEvent.getEventID());  //Event class implements Serializable
+                intent.putExtra("EventId", selectedEvent.getEventID());
+                intent.putExtra("OrganizerID", organizerId);
                 startActivity(intent);
+                finish();
             }
         });
 
@@ -136,6 +143,23 @@ public class OrganizerViewEventsActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrganizerViewEventsActivity.this, OrganizerHomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        OnBackPressedCallback onBackPressedCallback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                Intent intent = new Intent(OrganizerViewEventsActivity.this, OrganizerHomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        OrganizerViewEventsActivity.this.getOnBackPressedDispatcher().addCallback(this, onBackPressedCallback);
     }
 
     // loads the profile image from the saved user profile.
